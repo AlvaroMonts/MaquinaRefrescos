c192e0bc23ade3ac0e8eb05eb3cc6ae259ecf503
alvaro.montes <alvaro.montes@WKSALMPBB720.u-tad.es>
2016-10-21 10:14:49 +0200

    implementada clase datosficheros

---
+++ src/accesoDatos/DatosFicheros.java
@@ -0,0 +1,23 @@
+package accesoDatos;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class DatosFicheros {
+	private File archivoMonedas;
+	private File archivoProductos;
+	private HashMap<Integer,String> datosMonedas;
+	private HashMap<Integer,String> datosProductos;
+	
+	public HashMap<Integer,String> datosMonedas () {
+		
+		
+		return datosMonedas;
+	}
+	
+	public HashMap<Integer,String> datosProductos () {
+		
+		
+		return datosProductos;
+	}
+}
