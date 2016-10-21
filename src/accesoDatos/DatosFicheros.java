66f4a20b14713ff919bdb511c1c86113a8dff913
alvaro.montes <alvaro.montes@WKSALMPBB720.u-tad.es>
2016-10-21 10:53:37 +0200

    ya funciona la parte de depositos de monedas

--- src/accesoDatos/DatosFicheros.java
+++ src/accesoDatos/DatosFicheros.java
@@ -1,39 +1,58 @@
 package accesoDatos;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 import logicaRefrescos.Deposito;
 import logicaRefrescos.Dispensador;
 
 public class DatosFicheros {
-	private File archivoMonedas;
-	private File archivoProductos;
+	private File archivoMonedas = new File(
+			"C:/Users/alvaro.montes/git/adat-ud1-a03-maquina-refrescos-ficheros-AlvaroMonts/datosFicheros/datosMonedas");
+	private File archivoProductos = new File(
+			"C:/Users/alvaro.montes/git/adat-ud1-a03-maquina-refrescos-ficheros-AlvaroMonts/datosFicheros/datosProductos");
 	private HashMap<Integer, Deposito> datosMonedas;
 	private HashMap<String, Dispensador> datosProductos;
 
 	public HashMap<Integer, Deposito> obtenerDepositos() {
-		Deposito dep2eu = new Deposito("2 euros", 200, 1000);
-		Deposito dep1eu = new Deposito("1 euro", 100, 2000);
-		Deposito dep50cent = new Deposito("50 cent", 50, 3000);
-		Deposito dep20cent = new Deposito("20 cent", 20, 4000);
-		Deposito dep10cent = new Deposito("10 cent", 10, 5000);
-		Deposito dep5cent = new Deposito("5 cent", 5, 6000);
-		
-		datosMonedas.put(1, dep2eu);
-		datosMonedas.put(2, dep1eu);
-		datosMonedas.put(3, dep50cent);
-		datosMonedas.put(4, dep20cent);
-		datosMonedas.put(5, dep10cent);
-		datosMonedas.put(6, dep5cent);
-		
+		FileReader fr;
+		BufferedReader br;
+		try {
+			fr = new FileReader(archivoMonedas);
+			br = new BufferedReader(fr);
+			Deposito depositos[] = new Deposito[6];
+			String linea = new String();
+			for (int j = 0; (linea = br.readLine()) != null; j++) {
+				String sar[] = linea.split(",");
+				Deposito dep = new Deposito(sar[3], Integer.parseInt(sar[0]), Integer.parseInt(sar[1]));
+				depositos[j] = dep;
+			}
+
+			for (int i = 0; i < datosMonedas.size(); i++) {
+				datosMonedas.put(i, depositos[i]);
+			}
+/*
+			for (int i = 0; i < datosMonedas.size(); i++) {
+				datosMonedas.get(i);
+			}
+			*/
+			br.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return datosMonedas;
 	}
 
 	public HashMap<String, Dispensador> obtenerDispensadores() {
 		return datosProductos;
 	}
-
-	
 
 }
